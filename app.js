3c74ba1eff8d1f3ec60923a72d875ed2b35f4ef9
Hafsa302 <hafsafatima306@gmail.com>
2023-08-31 09:01:22 -0700

    Work Complete

---
+++ app.js
@@ -0,0 +1,599 @@
+//     solve chapter 1-20
+
+// chap 1
+
+// Q1:1.	Alert these following (individually):  
+// I.	First Name 
+
+
+// alert("Frist Name = hafsa");
+
+// // II.	Last Name 
+
+
+// alert("Last Name = fatima");
+
+// // III.	Email 
+
+
+// alert(email);
+
+// // IV.	Phone Number
+
+
+// alert("phone Number = 0302-264828");
+
+
+// V. 	Password 
+
+// 
+// alert("pasword =********");
+
+// Q2 : 2.	Correct this statement: alert"You're learning JavaScript!"; 
+ 
+// alert("you're learning Javascript!");
+
+// Q3 : Code an alert statement displaying any message you like. 
+// alert("Hello World");
+
+
+// chap-2
+
+// Q1 	Declare any variable in the camelCase format. 
+
+// var firstName ="john";
+// alert(firstName);
+
+// 2.	Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice.
+//  var a;
+//  a = "Monday"
+//  alert(a);
+
+// 3.	Declare the variable teamName and Alert your Team name. 
+// var teamName = "";
+// alert("john , ali , khan");
+
+// 4.	This statement has already been coded. var bestMan = "Charlie"; Assign the variable a new string. 
+
+// var bestMan = "Charlie";
+// bestMan = "john";
+// alert(bestMan);
+
+// chap-3
+
+
+// Q1: 1.	Declare a variable “caseQty” 
+
+// var caseQty = "";
+
+// // Q2:  2.	Assign to the variable caseQty, which has already been declared, the value 144. 
+
+// var caseQty = 144;
+// alert(caseQty);
+
+// Q3:	Rewrite this statement so the variable can be used in a math operation. var num = "9";
+
+// var num = "9";
+// var num1 = 10;
+// var nums = num + num1;
+// alert(nums);
+
+// Q4:	In one statement declare a variable. In a second statement assign it the sum of 2 numbers. 
+
+// var num = 45;
+// var total = num + 2;
+// console.log(total);
+
+// Q5:	What is the value of orderTotal? 
+
+// var merchTotal = 100; 
+// var shippingCharge = 10; 
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+
+// Q6: 6.	In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number. 
+
+
+// var number = 10;
+// var sum = 15;
+// var total = number + sum;
+// alert(total);
+
+
+// chap-4
+
+// Q1:Correct this statement. 
+
+// var productCost = 3.45;
+// alert(productCost) 
+
+// Q2: Rewrite this using camelCase. var Nameofband; 
+
+// var nameOfBand ;
+
+// Q3:In a single statement declare a legally-named variable and assign a number to it. 
+
+// var userResponse = 10;
+// alert(userResponse)
+
+// Q4: Declare a variable that is a combination of your first and last names. Use camelCase. 
+// var hafsaFatima ;
+
+// Q5:	List the legal and Illegal Variables. 
+
+// legal variables:
+// var username;
+// var fristName;
+// var n7;
+
+// illegal variables:
+// var 1;
+// var Lastname ;
+// var ++;
+
+// chap-5
+
+// Q1.	What is the name and symbol of the arithmetic operator that gives you the remainder when one number is divided by another? 
+
+// Modulus Operator    %
+
+// Q2.	What is the value of num? 
+
+// var num = 20 % 6; 
+// alert(num)
+
+// Q3.	In a single statement, declare the variable largeNum and assign it the result of 1,000 multiplied by 2,000. 
+
+// var largeNum = 1000 * 2000;
+// alert(largeNum);
+
+// Q4.	Assign to a variable the value represented by one variable subtracted from the value represented by another variable 
+
+// var  cost = 100;
+// var profit = 20;
+// alert(cost - profit);
+
+// 5.	Assign to a variable the remainder when one number is divided by another. The variable hasn't been declared beforehand. Make up the variable name. 
+
+// var percent = 80 / 5;
+// alert(percent);
+
+// 6.	Code an alert that displays the result of a multiplication on 2 numbers. 
+// var multiply = 8 * 5;
+// console.log(multiply)
+
+// chap-6
+
+// 1.	Code a short form of x = x + 1; Use either of the two legal expressions. 
+// var x = 10;
+// var x = x + 1;
+// alert(x);
+
+
+// 2.	If x has a value of 100, what is the fastest way to reduce it to 99 with a math expression? 
+
+// var x = 100;
+// x--
+// alert(x)
+
+// 3.	var x = 50; var y = x++; 
+// What is the value of y? 
+// value of y = 51
+
+
+// 4.	var y = 50; var z = --y; 
+// What is the value of z? 
+
+// value of z = 49
+
+// 5.	In a single statement, decrement num and assign its original value to newNum
+// var num = 1;
+// var newNum = --num;
+// alert(newNum);
+
+
+// 6.	In a single statement add 1 to a variable and assign its original value to another variable. 
+
+// var add;
+// add = 25
+
+// 7.	Assign a number value to a variable.  Increment the variable. 
+// Display the new value in an alert 
+
+// var number = 7;
+// number++
+// alert(number)
+
+
+// chap-7
+
+//Q1: var calculatedNum = 2 + (2 * 6); What is the value of calculatedNum? 
+// var calculatedNum = 2 +(2*6);
+// alert(calculatedNum);
+// 14
+
+//  2. var calculatedNum = (2 + 2) * 6; What is the value of calculatedNum? 
+
+//  var calculatedNum = (2 + 2) * 6 ;
+//  alert(calculatedNum)
+//  24
+
+// 3.	var calculatedNum = (2 + 2) * (4 + 2); 
+// What is the value of calculatedNum? 
+
+// var calculatedNum = (2 + 2) * (4 + 2); 
+// alert(calculatedNum)
+// 24
+
+// 4.	var calculatedNum = ((2 + 2) * 4) + 2; What is the value of calculatedNum? 
+
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// alert(calculatedNum)
+// 18
+
+// 3.	Write a statement that assigns to cost the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 56. 
+
+// var calculatedNum = (2 + 2)*(4 + 10);
+// alert(calculatedNum)
+
+// 4.	Write a statement that assigns to units the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 20. 
+
+// var calculatedNum = 2 + (2 * 4 ) + 10 ;
+// alert(calculatedNum)
+
+
+// 5.	Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing 5. 
+
+// var calculatedNum = 4/(2*4);
+// console.log(calculatedNum);
+
+// chap-8
+
+//Q1  What is the value of num? Include quotation marks. 
+	
+// var num = "2" + "2"; 
+// alert(num);
+// 22
+
+// Q2 What is the value of message? (Include the quotation marks.) Alert the statement
+
+// var message = ("Hello," + "Dolly"); 
+// alert(message)
+// // HelloDolly
+
+// 3.	alert("33" + 3); 
+// What message displays in the alert box? 
+
+// alert("33" + 3); 333
+
+// 4.	Write an alert that displays the concatenation of the two parts of "Pakistan Zindabad". 
+
+// var  msg = "Pakistan" + "Zindabad";
+// alert(msg)
+
+// 5.	Write a statement that assigns to a variable the concatenation of a string with a number 
+
+// var statement = "hello"+123;
+// alert(statement)
+
+// 6.	Assign strings to two variables. Then concatenate them and assign the result to a third variable. 
+
+// var a ="hello";
+// var b = "world";
+// var c = a + b;
+// alert(c)
+
+// chap-9
+
+// 1.	Code a prompt with the message "Enter first name". The user's response is assigned to firstName
+ 
+// var firstName = prompt("Enter first name");
+// alert(firstName)
+
+// 2.	Code a prompt with the message "Country?" and the default answer "China". The user's response is assigned to country. 
+// var ques ="Enter your country";
+// var defaultAnswer = "china";
+// var show = prompt(ques,defaultAnswer);
+// alert(show);
+
+// 3.	Correct this statement var yourName = prompt(Enter Your Name"); 
+// var name = prompt("Enter your name");
+// alert(name)
+
+// 4.	Code a prompt that specifies a string as the message Include a default input. 
+
+// var ques ="Enter your country";
+// var defaultAnswer = "china";
+// var show = prompt(ques,defaultAnswer);
+// alert(show);
+
+// 5.	Assign strings to two variables. 
+// Code a prompt specifying the first variable as the message and the second variable as the default response.
+//  Assign the user's response to a third variable. 
+
+// var age = "Enter your age";
+// var defaultAnswer = "25";
+// var show = prompt(age,defaultAnswer);
+
+// 6.	Display a prompt, including both a message and a default response. 
+// Display the user's response in an alert. 
+
+// var name = prompt("");
+// var defaultAnswer = "hafsa";
+// alert(name,defaultAnswer);
+
+
+// chap-10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// chap-21
+// Q1 Type the characters that are missing from this code.
+// var allLower = userInput.toLowerCase;
+
+// var name = prompt("Enter your name");
+// var name = name.toLowerCase();
+// console.log(name)
+
+// Q2 Convert the string represented by x to lower-case and assign the
+// result to the same variable.
+
+// var x = "Lorem ipsum dolor sit amet."
+// var x = x.toLowerCase();
+// console.log(x)
+
+
+// Q3 Convert the string represented by y to upper-case and assign the
+// result to the same variable.
+
+
+// var y = "Lorem ipsum dolor sit amet."
+// var y = y.toUpperCase();
+// console.log(y)
+
+//Q4 Convert the string represented by a variable to lower-case and
+// assign the result to a second variable that hasn't been declared
+// beforehand.
+
+// var a ="KARACHI";
+// a = a.toLowerCase();
+// console.log(a)
+
+
+//Q5 Convert the string represented by an array element to lower-case
+// and assign it to a variable that hasn't been declared beforehand.
+
+// var cityToCheck = prompt("Enter your city");
+// cityToCheck = cityToCheck.toLowerCase();
+// var cleanestCities = ["karachi","lahore","islamabad","peshawar"];
+// for (var i = 0; i <= 4; i++) {
+// if (cityToCheck === cleanestCities[i]) {
+//     alert("It's one of the cleanest cities");
+//   }
+//  }
+
+
+// Q6 Display in an alert the upper-case version of the string represented by a variable.
+
+// var name = prompt("enter your name");
+// var name = name.toUpperCase();
+// alert(name)
+
+// Q7 var cityName = “kaRacHi”;
+// Convert the string represented by a cityName in Capitalisation is
+// the writing of a word with its first letter in uppercase and the
+// remaining letters in lowercase.
+
+// var cityName ="Karachi"
+// alert(cityName)
+
+// chap: 22-25
+//Q1 "captain" has been assigned to variable “sameWords”. You want
+// to slice "ap" out of it.
+
+// var word ="captian";
+// console.log(word.slice(1,3))
+
+// var city = "karachi"
+// console.log(city.OfIndex)
+
+
+// Q3 The string "elephant" has been assigned to the variable animal.
+// Slice the four middle characters out of the string and assign it to
+// the variable seg, which hasn't been declared beforehand.
+
+// var animal = "elephant";
+// console.log(animal.slice(1,5))
+
+
+//Q4 Find the number of characters in the string represented by a
+// variable and assign the number to a second variable.
+// var a = "kjjxjxkks";
+// var num =123;
+// console.log(a.lenght)
+
+
+
+// Q5
+
+
+
+// var arr = [ "a", "b" ,"c","d","e"]
+// // console.log(arr.length)
+// console.log(arr.slice(0,5))
+// var arr = ['a','c','d','e']
+// console.log(arr)
+
+
+// Q6
+
+
+
+// var text = "To be or not to be";
+// var index = text.indexOf("be");
+// console.log(index)
+// 3
+
+
+// Q7
+
+// var text = "To be or not to be";
+// var index = text.lastindexOf("be");
+// console.log(index)
+// lastindex is not a function
+
+
+// Q8
+// var a = 'go';
+// console.log(a.indexOf("go"))
+
+
+
+
+// Q9
+
+// var age = 24;
+// var 
+
+// Q10
+
+// var a ="abcde";
+// console.log(a.indexOf(0,2))
+
+
+// Q11Find the 10th character in the string represented by text andassign it to the variable cha, which hasn't been declared beforehand.
+
+// var arr1 = [ "a", "b" ,"c","d","e", "f" , "g","i","j","k","l"]
+// arr1[10] = cha;
+// console.log(arr1)
+
+// Q12 Find the last character in the string represented by str and
+// assign it to x, which hasn't been declare
+
+// var arr1 = [ "a", "b" ,"c","d","e", "f" , "g","i","j","k","l"]
+// arr1.push("x")
+// console.log(arr1)
+
+
+// Q13 Find the the 5th character in a string represented by input and assign it to cha, which hasn't been declared beforehand.
+
+var arr1 = [ "a", "b" ,"c","d","e", "f" , "g","i","j","k","l"]
+console.log(arr1.indexOf(5,0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
